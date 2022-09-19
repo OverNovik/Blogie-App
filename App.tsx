02cec38b92993a68a05a2ba15933c60a3dddf877
Aliaxander Novikov <nerevarin11@gmail.com>
2022-09-19 18:01:41 +0300

    feat: add TutorialScreen

--- App.tsx
+++ App.tsx
@@ -1,28 +1,23 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import TutorialScreen from './screens/TutorialScreen/TutorialScreen';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+const App: React.FC = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Blogie</Text>
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="TutorialScreen" component={TutorialScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
---
+++ screens/TutorialScreen/TutorialScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './style';
+
+const TutorialScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>TutorialScreen</Text>
+    </View>
+  );
+};
+
+export default TutorialScreen;
