6acfdc9d914e16178b655732cc8fee6e050e07da
Aliaxander Novikov <nerevarin11@gmail.com>
2022-09-20 23:09:07 +0300

    feat: add Button component

---
+++ components/UI/Button/Button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Pressable, Text, View} from 'react-native';
+import {styles} from './style';
+
+interface Props {
+  children: React.ReactNode;
+  onPress: () => void;
+  styleProp: any;
+}
+
+const Button: React.FC<Props> = ({children, onPress, styleProp}) => {
+  return (
+    <View style={styleProp}>
+      <Pressable
+        onPress={onPress}
+        style={({pressed}) => pressed && styles.pressed}>
+        <View style={[styles.button, styleProp]}>
+          <Text style={[styles.buttonText, styleProp]}>{children}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Button;
