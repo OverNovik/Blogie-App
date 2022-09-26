64bce64eaacc38700736448766720e99d0e80efe
Aliaxander Novikov <nerevarin11@gmail.com>
2022-09-26 16:17:10 +0300

    feat: add LoginScreen UI

--- screens/LoginScreen/LoginScreen.tsx
+++ screens/LoginScreen/LoginScreen.tsx
@@ -1,11 +1,61 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+/* eslint-disable @typescript-eslint/no-shadow */
+import React, {useState} from 'react';
+import {Text, View, Image, TextInput} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+import Button from '../../components/UI/Button/Button';
+import {GlobalStyles} from '../../constants/style';
+import {styles} from './style';
 
 const LoginScreen: React.FC = () => {
+  const [email, setEmail] = useState<string | null>('');
+  const [password, setPassword] = useState<string | null>('');
+  const [toggleCheckBox, setToggleCheckBox] = useState<boolean | undefined>(
+    false,
+  );
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Image source={require('../../assets/logo.png')} style={styles.image} />
+        <Text style={styles.label}>Email</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={email => setEmail(email)}
+          />
+        </View>
+        <Text style={styles.label}>Password</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.textInput}
+            secureTextEntry={true}
+            onChangeText={password => setPassword(password)}
+          />
+        </View>
+        <View style={styles.utilsContainer}>
+          <View style={styles.checkBoxContainer}>
+            <CheckBox
+              disabled={false}
+              value={toggleCheckBox}
+              onValueChange={newValue => setToggleCheckBox(newValue)}
+              tintColors={{true: GlobalStyles.colors.violet20}}
+            />
+            <Text>Remember me?</Text>
+          </View>
+          <Button styleProp={styles.secondBtn} onPress={() => {}}>
+            Forgot Password?
+          </Button>
+        </View>
+        <Button onPress={() => {}}>Sign in</Button>
+        <View style={styles.haveAccount}>
+          <Text>Don’t have an account? </Text>
+          <Button
+            styleProp={[styles.secondBtn, styles.signUpBtn]}
+            onPress={() => {}}>
+            Sign up
+          </Button>
+        </View>
+      </View>
+    </>
   );
 };
 
---
+++ screens/LoginScreen/style.ts
@@ -0,0 +1,67 @@
+import {StyleSheet} from 'react-native';
+import {GlobalStyles} from '../../constants/style';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: GlobalStyles.colors.white,
+    alignItems: 'center',
+  },
+  image: {
+    marginTop: 92,
+    marginBottom: 41.27,
+  },
+  inputView: {
+    backgroundColor: GlobalStyles.colors.white10,
+    borderRadius: 15,
+    width: 327,
+    height: 49,
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  label: {
+    fontFamily: 'DM Sans',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: 16,
+    lineHeight: 21,
+    color: GlobalStyles.colors.gray30,
+    marginBottom: 10,
+  },
+  textInput: {
+    width: 327,
+    height: 49,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  utilsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  checkBoxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: '25%',
+  },
+  secondBtn: {
+    backgroundColor: GlobalStyles.colors.white,
+    color: GlobalStyles.colors.violet20,
+    fontFamily: 'DM Sans',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: 13,
+    lineHeight: 17,
+    width: 108,
+  },
+  haveAccount: {
+    marginTop: '40%',
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    justifyContent: 'center',
+  },
+  signUpBtn: {
+    width: 50,
+  },
+});
